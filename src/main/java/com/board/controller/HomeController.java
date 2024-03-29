3ae5ff55d805dee958606e616551388b6556df2c
pmj8298 <pmj8298@naver.com>
2024-03-29 13:47:25 +0900

    수정

---
+++ src/main/java/com/board/controller/HomeController.java
@@ -0,0 +1,51 @@
+package com.board.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.board.user.domain.UserVo;
+import com.board.user.mapper.UserMapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	private UserMapper userMapper;
+
+	// http://localhost:9090
+	@RequestMapping("/")
+	public String home() {
+		return "home";
+	}
+	
+	// /loginForm
+	@RequestMapping("/loginForm")
+	public ModelAndView loginForm() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("users/login");
+		return mv;
+	}
+	
+	// /login(userid=U002, passwd=1234)
+	@RequestMapping("/login")
+	public ModelAndView login(HttpServletRequest request) {
+		
+		String userid = request.getParameter("userid");
+		String passwd = request.getParameter("passwd");
+		
+		UserVo userVo  = userMapper.login(userid, passwd);
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("login", userVo);
+		session.setMaxInactiveInterval(30*60);
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("redirect:/");
+		return mv;
+	}
+}
